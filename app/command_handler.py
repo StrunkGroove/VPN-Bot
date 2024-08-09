48f012d98e008fb2fa852a63e2c212fea6960a26
StrunkGroove <jmarkj@mail.ru>
2024-08-09 13:12:39 +0300

    first steps ...

--- app/command_handler.py
+++ app/command_handler.py
@@ -4,7 +4,7 @@ from aiogram.filters.command import Command
 from aiogram.types import Message
 
 from commands import commands
-from func_outline import OutlineManager
+from func_outline import OutlineManager, OneGigabyte, bytes_to_gb
 
 
 router = Router()
@@ -17,11 +17,35 @@ async def command_start_handler(message: Message) -> None:
 
 @router.message(Command(commands["access_key"]["key"]))
 async def command_get_access_key(message: Message) -> None:
+    user_id = message.from_user.id
     manager = OutlineManager()
-    response = await manager.get_keys()
-    await message.answer("Хер тебе ! Пока что)")
+    user_data = await manager.get_key(user_id)
+    if user_data == OutlineManager.not_foud:
+        user_data = await manager.create_key(
+            user_id,
+            name=message.from_user.username,
+            limit={"bytes": OneGigabyte.value},
+        )
+    message_text = commands["access_key"]["message"].format(
+        limit=OneGigabyte.name, 
+        key=user_data["accessUrl"]
+    )
+    await message.answer(message_text, parse_mode="MarkdownV2")
 
 
 @router.message(Command(commands["stastitics"]["key"]))
 async def command_get_stastitics(message: Message) -> None:
-    await message.answer(f"Hello, get_stastitics!")
+    manager = OutlineManager()
+    user_id = message.from_user.id
+    transferred_data = await manager.get_transferred_data_per_user(user_id)
+    user_data = await manager.get_key(user_id)
+    if data_limit := user_data.get("dataLimit", None):
+        message_text = commands["stastitics"]["message"].format(
+            transferred_data=transferred_data,
+            limit=bytes_to_gb(data_limit["bytes"])
+        )
+    else:
+        message_text = commands["stastitics"]["message_without_limit"].format(
+            transferred_data=transferred_data
+        )
+    await message.answer(message_text)
--- app/func_outline.py
+++ app/func_outline.py
@@ -1,24 +1,81 @@
 from os import getenv
+from typing import Union
 from urllib.parse import urljoin
 
 import aiohttp
 from aiohttp import TCPConnector
 
 
-class OutlineServerErrorException(Exception):
+def bytes_to_gb(integer: int) -> float:
+    return integer / (1024 ** 3)
+
+
+class OneGigabyte:
+    name = "1 Gb"
+    value = 1_073_741_824
+
+
+class OutlineServerException(Exception):
     pass
 
 
 class OutlineManager:
+    not_foud = "Not found"
+
     def __init__(self) -> None:
         self.api_url = getenv("API_URL_VPN")
         self.secret_string = getenv("SECRET_VPN_STRING")
 
-    async def get_keys(self, timeout: int = None):
+    async def get_data(
+        self, url: str, method = "get", **kwargs
+    ) -> Union[dict, str]:
+        async with aiohttp.ClientSession(
+            connector=TCPConnector(ssl=False)
+        ) as session:
+            async with session.request(
+                method, url, **kwargs
+            ) as response:
+                if response.status == 404:
+                    return self.not_foud
+                if response.status >= 200 and response.status < 300:
+                    return await response.json()
+                raise OutlineServerException()
+
+    async def get_keys(self, timeout: int = None) -> Union[dict, str]:
         url = urljoin(self.api_url, f"/{self.secret_string}/access-keys/")
-        async with aiohttp.ClientSession(connector=TCPConnector(ssl=False)) as session:
-            async with session.get(url, timeout=timeout) as response:
-                if response.status == 200:
-                    data = await response.json()
-                    if "accessKeys" in data:
-                        return data
+        result = await self.get_data(url, timeout=timeout)
+        print(result)
+        return result
+
+    async def get_key(self, key_id: str, timeout: int = None) -> Union[dict, str]:
+        url = urljoin(self.api_url, f"/{self.secret_string}/access-keys/{key_id}")
+        result = await self.get_data(url, timeout=timeout)
+        return result
+
+    async def create_key(
+        self, key_id: int, timeout: int = None, **kwargs
+    ):
+        """"
+        name: str = None,
+        method: str = None,
+        password: str = None,
+        port: int = None,
+        limit: dict = {"bytes": data_limit}
+        """
+        payload = {**kwargs}
+        url = urljoin(self.api_url, f"/{self.secret_string}/access-keys/{key_id}",)
+        result = await self.get_data(url, "put", timeout=timeout, json=payload)
+        return result
+
+    async def get_transferred_data(self, timeout: int = None) -> dict:
+        url = urljoin(self.api_url, f"/{self.secret_string}/metrics/transfer",)
+        result = await self.get_data(url, timeout=timeout)
+        return result
+
+    async def get_transferred_data_per_user(
+        self, key_id: int, timeout: int = None
+    ) -> float:
+        result = await self.get_transferred_data(timeout)
+        if transferred_data := result.get(key_id):
+            return bytes_to_gb(transferred_data["bytes"])
+        return 0
